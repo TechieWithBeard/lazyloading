eb4b773ee84cc50db30da37484ddd6cb5daac797
vishnu Thankappan <vishnu.thankappan@maistering.com>
2021-12-08 13:16:02 +0530

    fix(package): package upgrade

--- src/app/click-load-page/click-load-page.component.spec.ts
+++ src/app/click-load-page/click-load-page.component.spec.ts
@@ -1,25 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ClickLoadPageComponent } from './click-load-page.component';
 
 describe('ClickLoadPageComponent', () => {
-  let component: ClickLoadPageComponent;
-  let fixture: ComponentFixture<ClickLoadPageComponent>;
+    let component: ClickLoadPageComponent;
+    let fixture: ComponentFixture<ClickLoadPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ClickLoadPageComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [RouterModule.forRoot([]), RouterTestingModule],
+            declarations: [ClickLoadPageComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ClickLoadPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ClickLoadPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
